44c7d2f2b674290d5bb4944e4f2c5d1bf60bddd7
haoshaochun <haoshaochun@souche.com>
2023-07-06 19:39:27 +0800

    fix: prompt

--- src/docqa/app/prompter.py
+++ src/docqa/app/prompter.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 
 class Prompter:
@@ -24,14 +24,14 @@ class Prompter:
     def build_docqa_prompt(
         self,
         q: str,
-        docs: Optional[List[str]],
+        docs: Optional[List[Dict]],
     ) -> str:
         if docs is None:
             prompt = "你现在是一台服务器，你的服务不可用了。用户正在访问，你这样回复用户："
             return prompt
-        doc_str = "\n".join(docs)
+        lst = [v["content"] for v in docs]
+        doc_str = "\n".join(lst)
         prompt = f"""根据给定文本回答问题，如果没有答案或答案不确定，回复“未涉及”。
-
 给定文本：
 ```
 {doc_str}
